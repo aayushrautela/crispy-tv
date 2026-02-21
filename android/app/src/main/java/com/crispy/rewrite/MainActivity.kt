7034d4929001bdc3af742fd78935976481364378
Aayush Rautela <rautela4488@gmail.com>
2026-02-21 20:22:20 +0100

    Remove unused FontWeight import from MainActivity.kt

--- android/app/src/main/java/com/crispy/rewrite/MainActivity.kt
+++ android/app/src/main/java/com/crispy/rewrite/MainActivity.kt
@@ -123,7 +123,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
