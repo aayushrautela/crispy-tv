8479189af3cb4b8ffffc4f390931afcd1f64481c
Aayush Rautela <rautela4488@gmail.com>
2026-02-24 10:59:47 +0100

    Fix import formatting in DetailsHeader

--- android/app/src/main/java/com/crispy/tv/details/DetailsHeader.kt
+++ android/app/src/main/java/com/crispy/tv/details/DetailsHeader.kt
@@ -12,7 +12,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.widthimport androidx.compose.material.icons.Icons
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.CheckCircle
 import androidx.compose.material.icons.filled.PlayArrow
