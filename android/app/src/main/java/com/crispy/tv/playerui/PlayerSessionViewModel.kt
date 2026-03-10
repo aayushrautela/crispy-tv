e8fbae17933e7e2778f95380a8d484d73e3984d3
Aayush Rautela <rautela4488@gmail.com>
2026-03-10 23:59:52 +0100

    chore: add import for `kotlinx.coroutines.isActive`

--- android/app/src/main/java/com/crispy/tv/playerui/PlayerSessionViewModel.kt
+++ android/app/src/main/java/com/crispy/tv/playerui/PlayerSessionViewModel.kt
@@ -26,6 +26,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 
 @Immutable
