086323b82aecceeb4f533a7eb5867b9726f012ca
Aayush Rautela <rautela4488@gmail.com>
2026-03-14 00:27:36 +0100

    Remove unused internal Compose weight import causing compilation error

--- android/app/src/main/java/com/crispy/tv/search/SearchScreen.kt
+++ android/app/src/main/java/com/crispy/tv/search/SearchScreen.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
