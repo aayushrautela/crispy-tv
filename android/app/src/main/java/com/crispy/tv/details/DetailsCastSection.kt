3877c9ae256503bf96193b665731f966561b2814
Aayush Rautela <rautela4488@gmail.com>
2026-04-07 12:00:22 +0200

    Fix missing Compose imports in DetailsCastSection

--- android/app/src/main/java/com/crispy/tv/details/DetailsCastSection.kt
+++ android/app/src/main/java/com/crispy/tv/details/DetailsCastSection.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
@@ -20,6 +21,7 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
