4f25f587eb2af9bccee0d9f6e6f2d952ff31eb09
Aayush Rautela <rautela4488@gmail.com>
2026-05-15 11:04:55 +0200

    Bump AGP version from 9.2.0-alpha13 to 9.2.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "9.2.0-alpha13" apply false
-    id("com.android.library") version "9.2.0-alpha13" apply false
+    id("com.android.application") version "9.2.1" apply false
+    id("com.android.library") version "9.2.1" apply false
     id("org.jetbrains.kotlin.jvm") version "2.3.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
 }
