16953c7041d73554de03df960c28922dc77accc2
Aayush Rautela <rautela4488@gmail.com>
2026-07-30 12:08:32 +0200

    fix(details): restore Composable import in DetailsBody.kt
    Previous commit removed androidx.compose.runtime.Composable along with
    remember, but Composable is still needed by DetailsReviewPlaceholder
    (line 419) below detailsBodyContent. Restores the import.

--- android/app/src/main/java/com/crispy/tv/details/DetailsBody.kt
+++ android/app/src/main/java/com/crispy/tv/details/DetailsBody.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
