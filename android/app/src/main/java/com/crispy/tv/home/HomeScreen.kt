672bf90a0d25e6af23c724372c8ef054e6c79e3f
Aayush Rautela <rautela4488@gmail.com>
2026-08-06 16:28:22 +0200

    fix: missing Row import and repeat() not composable-safe
    - Add Row import
    - Replace repeat(4) with List(4) which works in @Composable context

--- android/app/src/main/java/com/crispy/tv/home/HomeScreen.kt
+++ android/app/src/main/java/com/crispy/tv/home/HomeScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -211,7 +212,7 @@ private fun HomeHeaderSectionsItem(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.spacedBy(8.dp),
         ) {
-            repeat(4) {
+            List(4) {
                 Box(
                     modifier = Modifier
                         .width(80.dp)
