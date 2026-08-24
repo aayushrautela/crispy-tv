83297f52a56010fcd15854366cd0ec5501166669
Aayush Rautela <rautela4488@gmail.com>
2026-08-24 05:44:40 +0200

    fix(app): remember static rotation state in AiSearchButton (UnrememberedMutableState)

--- android/app/src/main/java/com/crispy/tv/search/SearchTopBar.kt
+++ android/app/src/main/java/com/crispy/tv/search/SearchTopBar.kt
@@ -27,8 +27,9 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.State
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -149,9 +150,7 @@ fun AiSearchButton(
             label = "ai_sparkle_rotation",
         )
     } else {
-        object : State<Float> {
-            override val value = 0f
-        }
+        remember { mutableStateOf(0f) }
     }
 
     Box(
