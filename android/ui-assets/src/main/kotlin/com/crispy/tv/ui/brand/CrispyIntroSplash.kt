9d03a47635428a7a7ec42389585749ec13bab51c
Aayush Rautela <rautela4488@gmail.com>
2026-09-09 23:33:42 +0200

    fix(brand): repair mangled Composable import in CrispyIntroSplash

--- android/ui-assets/src/main/kotlin/com/crispy/tv/ui/brand/CrispyIntroSplash.kt
+++ android/ui-assets/src/main/kotlin/com/crispy/tv/ui/brand/CrispyIntroSplash.kt
@@ -13,7 +13,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
-import androidx.compose.runtime.Composableimport androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
